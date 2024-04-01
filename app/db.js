932c0b6cd889bb2cf0a563469ffd59f07a6983e5
Tushar Karamchandani <tusharkaramchandani25@gmail.com>
2024-04-01 08:21:15 +0530

    added changes for staging env

--- app/db.js
+++ app/db.js
@@ -7,6 +7,9 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: process.env.NODE_ENV !== "development" ? true : false,
+    },
   }
 );
 
